2ce91c7a1d624cb9e13597da5f6c058003a50aa0
keshavmishra2 <87552836+keshavmishra2@users.noreply.github.com>
2022-04-26 14:51:22 +0530

    done

--- counter_app/src/Components/counter.jsx
+++ counter_app/src/Components/counter.jsx
@@ -3,29 +3,39 @@ import React, { useState } from 'react';
 import './counter.css'
 export const Counter = () =>{
   
-    const [count, setCount] = useState('1')
+    const [count, setCount] = useState(1)
 
     const decrement = () =>{
-       setCount(count-1)
+       setCount(count - 1)
     }
     const increment = () =>{
-        setCount(count+1)
+        setCount(count + 1)
     }
-
+      
+    const setvalue = (e) =>{
+        setCount(e.target.value)
+    }
     return(
+        <>
+         {/* <input className='myinpt' onChange={setvalue}  type="number" placeholder='Enter the start number ' ></input> */}
+
         <div className='counterdiv'>
+
+
+
          <button onClick={decrement} className='btn1'>
            -
          </button>
 
-         <h2 className='count2'>
+         <input className='count2' onChange={setvalue} value="input" type="number">
             {count}
-         </h2>
+         </input>
 
          <button onClick={increment} className='btn2'>
              +
          </button>
         </div>
+        </>
     )
 }
 
